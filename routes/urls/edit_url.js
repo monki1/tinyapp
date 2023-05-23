a329207561aff0e4dc1128adabd753603912ed5b
monki1 <114178697+monki1@users.noreply.github.com>
2023-05-23 06:14:40 -0400

    edit url get

---
+++ routes/urls/edit_url.js
@@ -0,0 +1,30 @@
+const urlDatabase = require('../../src/persistence/database');
+const users = require('../../src/persistence/users');
+
+const getHandler = (req, res) =>{
+  const shortURL = req.params.shortURL;
+  if (!urlDatabase.shortUrlExists(shortURL)) {
+    return res.status(401).send('Please log in to edit URLs.');
+  }
+  const user = users.getUserByCookie(req);
+  if (!user) {
+    return res.status(401).send('Please log in to edit URLs.');
+  }
+  console.log(user.id, shortURL);
+  if (! urlDatabase.userOwnsShortURL(user.id, shortURL)) {
+    return res.status(403).send('You do not have permission to edit this URL.');
+  }
+
+  const templateVars = {
+    id: shortURL,
+    longURL: urlDatabase.longURL,
+    user: user,
+  };
+
+  return res.render('urls_show', templateVars);
+
+
+
+};
+
+module.exports = {get: getHandler, };
